1e6dc11c5e296a6fe55b8030de9e8ab5f7342803
nickJ <nicknotjustin@gmail.com>
2019-01-25 11:16:33 -0600

    Initial commit. State is currently being held in game.

--- src/components/game.js
+++ src/components/game.js
@@ -5,14 +5,33 @@ import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
+export default class Game extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            feedback: "Make your guess!",
+            count: 3,
+            guesses: [10, 15, 25],
+            answer: 50
+        };
+    }
+
+    addGuess(input){
+        this.setState({
+            guesses: [...this.state.guesses, input]
+        })
+    }
+
+    
+    render(){
     return (
         <div>
             <Header />
-            <GuessSection feedback="Make your guess!" />
-            <GuessCount count={3} />
-            <GuessList guesses={[10, 15, 25]} />
+            <GuessSection feedback={this.state.feedback}
+                          handleSubmit = {this.addGuess} />
+            <GuessCount count={this.state.count} />
+            <GuessList guesses={this.state.guesses} />
         </div>
     );
 }
-
+}
